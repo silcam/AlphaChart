0cae571dd32855fe4324acb55b1bd03cf6923b44
Rick Conrad <richardjconrad@gmail.com>
2019-10-16 15:37:12 +0100

    Rearrange css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./components.css";
+import "./alphatable.css";
 import App from "./App";
 // import * as serviceWorker from './serviceWorker';
 
