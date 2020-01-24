ddf522a6a5a855979ea1d7f3fc9a3f71fffb9aee
Rick Conrad <richardjconrad@gmail.com>
2020-01-24 13:47:25 +0100

    wip

--- client/src/components/common/OptionButton.tsx
+++ client/src/components/common/OptionButton.tsx
@@ -1,9 +1,12 @@
 import React, { useState, ReactNode } from "react";
+import useDetectOutsideClick from "./useDetectOutsideClick";
+import LnkBtn from "./LnkBtn";
 
 interface IProps {
   buttonText: string;
   onMainClick: () => void;
   renderContextMenu: (props: { hideMenu: () => void }) => ReactNode;
+  lnkBtn?: boolean;
 }
 
 export default function OptionButton(props: IProps) {
@@ -27,9 +30,16 @@ export function OptionButtonSimple(props: Omit<IProps, "onMainClick">) {
   const [showMenu, setShowMenu] = useState(false);
   const toggleShowMenu = () => setShowMenu(!showMenu);
 
+  let divRef = React.createRef<HTMLDivElement>();
+  useDetectOutsideClick(divRef, () => setShowMenu(false));
+
   return (
-    <div className="compOptionButtonSimple">
-      <button onClick={toggleShowMenu}>{`${props.buttonText} ▼`}</button>
+    <div className="compOptionButtonSimple" ref={divRef}>
+      {props.lnkBtn ? (
+        <LnkBtn text={`${props.buttonText} ▼`} onClick={toggleShowMenu} />
+      ) : (
+        <button onClick={toggleShowMenu}>{`${props.buttonText} ▼`}</button>
+      )}
       {showMenu && (
         <div className="contextMenu">
           {props.renderContextMenu({ hideMenu: () => setShowMenu(false) })}
